6cb69fbf406b7e6590dabb1f814d8c60c8e2d8f8
drjaykuro <gagarigajonathan7@gmail.com>
2026-09-06 05:40:22 +0100

    Route AddTask data through offline-first store

--- src/AddTask.jsx
+++ src/AddTask.jsx
@@ -3,7 +3,7 @@ import {
   ArrowLeft, Heart, Briefcase, BookOpen, Dumbbell, ClipboardList, Utensils,
   MoreHorizontal, Flame, Ban, RotateCw, CalendarDays, X,
 } from 'lucide-react'
-import { supabase } from './supabaseClient'
+import { offlineSupabase as supabase } from './offlineStore'
 import { calculateBlockSize, SIZE_POINTS, DIFFICULTY_MULTIPLIER, CATEGORY_COLORS, todayStr } from './blocksLogic'
 import './AddTask.css'
 
